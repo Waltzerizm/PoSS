9c76f46f26d60957dbf676addfc89b79ce89b4f1
Kalpokas_Simonas <simonas.kalpokas@gmail.com>
2022-11-27 17:44:56 +0200

    Add comments to most bussiness owner endpoints

--- PoSS/Controllers/DiscountController.cs
+++ PoSS/Controllers/DiscountController.cs
@@ -4,27 +4,93 @@ using PoSS.Models;
 
 namespace PoSS.Controllers
 {
+    /// <summary>
+    /// Controller for managing discounts.
+    /// </summary>
     [ApiController]
-    [Route("[controller]/")]
-    public class DiscountController : CRUDController<Discount, DiscountDto>
+    [Route("{tenantId}/[controller]")]
+    public class DiscountController : ControllerBase
     {
+        /// <summary>
+        /// Endpoint to get multiple discounts.
+        /// </summary>
+        /// <param name="pageSize">The maximum amount of discounts in response.</param>
+        /// <param name="pageNumber">The page number of discounts to return.</param>
+        /// <param name="tenantId">Id of the store.</param>
+        /// <returns>A list of discounts.</returns>
+        [HttpGet("{pageSize}/{pageNumber}")]
+        public ActionResult<IEnumerable<DiscountDto>> Get(int tenantId, int pageSize, int pageNumber)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Endpoint to get a single discount.
+        /// </summary>
+        /// <param name="id">Id of the discount to get</param>
+        /// <param name="tenantId">Id of the store.</param>
+        /// <returns>Discount with the given id.</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Discount> Get(int tenantId, int id)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Endpoint to create a new discount.
+        /// </summary>
+        /// <param name="discount">Discount to create.</param>
+        [HttpPost]
+        public IActionResult Post([FromBody] DiscountDto discount)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Endpoint to update a discount.
+        /// </summary>
+        /// <param name="id">Id of the discount to update.</param>
+        /// <param name="discount">Discount to update.</param>
+        /// <param name="tenantId">Id of the store.</param>
+        [HttpPut("{id}")]
+        public IActionResult Put(int tenantId, int id, [FromBody] DiscountDto discount)
+        {
+            return Ok();
+        }
+
         // BO11.	As a business owner, I want to assign a discount to a product or a service so that the discount is automatically applied when the product or service is purchased.
+        /// <summary>
+        /// Endpoint to assign a discount to a product or service.
+        /// </summary>
+        /// <param name="tenantId">Id of the store.</param>
+        /// <param name="dto"></param>
         [HttpPost("AssignDiscountToItem")]
-        public IActionResult AssignDiscountToItem()
+        public IActionResult AssignDiscountToItem(int tenantId, AssignDiscountToItemDTO dto)
         {
             return Ok();
         }
 
         // BO12.	As a business owner, I want to assign a discount to a brand, so that the discount is automatically applied to any product made by the selected brand.
+        /// <summary>
+        /// Endpoint to assign a discount to a brand.
+        /// </summary>
+        /// <param name="tenantId">Id of the store.</param>
+        /// <param name="dto"></param>
         [HttpPost("AssignDiscountToBrand")]
-        public IActionResult AssignDiscountToBrand()
+        public IActionResult AssignDiscountToBrand(int tenantId, AssignDiscountToBrandDTO dto)
         {
             return Ok();
         }
 
         // BO13.	As a business owner, I want to assign a discount to a category, so that the discount is automatically applied to any product from the selected category.
+        /// <summary>
+        /// Endpoint to assign a discount to a category.
+        /// </summary>
+        /// <param name="tenantId">Id of the store.</param>
+        /// <param name="dto"></param>
         [HttpPost("AssignDiscountToCategory")]
-        public IActionResult AssignDiscountToCategory()
+        public IActionResult AssignDiscountToCategory(int tenantId, AssignDiscountToCategoryDTO dto)
         {
             return Ok();
         }
--- PoSS/Controllers/ServiceController.cs
+++ PoSS/Controllers/ServiceController.cs
@@ -8,8 +8,66 @@ namespace PoSS.Controllers
     /// Controller for managing services.
     /// </summary>
     [ApiController]
-    [Route("[controller]")]
-    public class ServiceController : CRUDController<Service, ServiceDto>
+    [Route("{tenantId}/[controller]")]
+    public class ServiceController : ControllerBase
     {
+        /// <summary>
+        /// Endpoint to get multiple services.
+        /// </summary>
+        /// <param name="pageSize">The maximum amount of services in response.</param>
+        /// <param name="pageNumber">The page number of services to return.</param>
+        /// <param name="tenantId">Id of the store.</param>
+        /// <returns>A list of services.</returns>
+        [HttpGet("{pageSize}/{pageNumber}")]
+        public ActionResult<IEnumerable<ServiceDto>> Get(int tenantId, int pageSize, int pageNumber)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Endpoint to get a single service.
+        /// </summary>
+        /// <param name="id">Id of the service to get</param>
+        /// <param name="tenantId">Id of the store.</param>
+        /// <returns>Service with the given id.</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Service> Get(int tenantId, int id)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Endpoint to create a new service.
+        /// </summary>
+        /// <param name="service">Service to create.</param>
+        [HttpPost]
+        public IActionResult Post([FromBody] ServiceDto service)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Endpoint to update a service.
+        /// </summary>
+        /// <param name="id">Id of the service to update.</param>
+        /// <param name="service">Service to update.</param>
+        /// <param name="tenantId">Id of the store.</param>
+        [HttpPut("{id}")]
+        public IActionResult Put(int tenantId, int id, [FromBody] ServiceDto service)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Endpoint to delete a service.
+        /// </summary>
+        /// <param name="id">Id of the service to delete.</param>
+        /// <param name="tenantId">Id of the store.</param>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int tenantId, int id)
+        {
+            return Ok();
+        }
     }
 }
